7c6eb3e360798f5b59622a8103f71171f38e8ceb
Spencer Alves <impiaaa@gmail.com>
2012-11-01 20:35:19 -0400

    Cleaned up and updated for latest updates

--- equipregions.py
+++ equipregions.py
@@ -8,12 +8,11 @@ allitems = schema.get_items()
 regionsDict = {}
 
 def add_region(itemname, region):
-	global regionsDict
 	if region in regionsDict:
 		regionsDict[region].append(itemname)
 	else:
-		regionsDict[region] = []
-		regionsDict[region].append(itemname)
+		regionsDict[region] = [itemname]
+		print 'Added region', region
 
 for item in allitems:
 	item = allitems[item]
@@ -29,8 +28,14 @@ for item in allitems:
 			for region in regions:
 				if region != 'hat':
 					add_region(itemname, region)
-		if 'prefab' in item and item['prefab'] == 'award_medal':
-			region = 'medal'
+		if 'prefab' in item:
+			if item['prefab'] == 'award_medal':
+				region = 'medal'
+			elif item['prefab'] == 'zombie':
+				region = 'zombie_body'
+			else:
+				print 'Prefab', item['prefab'], 'has unknown region'
+				continue
 			add_region(itemname, region)
 
 ### Some output fixes here ###
@@ -62,7 +67,7 @@ medalsException = ['First Place - ETF2L Highlander Tournament',
 				   'UGC Highlander Tin 2nd Place',
 				   'UGC Highlander Tin 3rd Place',
 				   'UGC Highlander 1st Place European Platinum',
-                   'UGC Highlander 1st Place European Silver',
+				   'UGC Highlander 1st Place European Silver',
 				   'UGC Highlander 1st Place European Steel',
 				   'UGC Highlander 1st Place North American Platinum',
 				   'UGC Highlander 1st Place North American Silver',
@@ -85,58 +90,96 @@ medalsException = ['First Place - ETF2L Highlander Tournament',
 				   'UGC Highlander 3rd Place North American Steel',
 				   'UGC Highlander 3rd Place South American Platinum',
 				   'UGC Highlander 3rd Place South American Steel',
-				   'UGC Highlander Steel Participant']
+				   'UGC Highlander Steel Participant',
+				   'ESL Season VI Division 1 1st Place',
+				   'ESL Season VI Division 1 2nd Place',
+				   'ESL Season VI Division 1 3rd Place',
+				   'ESL Season VI Division 1 Participant',
+				   'ESL Season VI Division 2 1st Place',
+				   'ESL Season VI Division 2 2nd Place',
+				   'ESL Season VI Division 2 3rd Place',
+				   'ESL Season VI Division 2 Participant',
+				   'ESL Season VI Division 3 1st Place',
+				   'ESL Season VI Division 3 2nd Place',
+				   'ESL Season VI Division 3 3rd Place',
+				   'ESL Season VI Division 3 Participant',
+				   'ESL Season VI Division 4 1st Place',
+				   'ESL Season VI Division 4 2nd Place',
+				   'ESL Season VI Division 4 3rd Place',
+				   'ESL Season VI Division 4 Participant',
+				   'ESL Season VI Division 5 1st Place',
+				   'ESL Season VI Division 5 2nd Place',
+				   'ESL Season VI Division 5 3rd Place',
+				   'ESL Season VI Division 5 Participant',
+				   'ESL Season VI Premier Division 1st Place',
+				   'ESL Season VI Premier Division 2nd Place',
+				   'ESL Season VI Premier Division 3rd Place',
+				   'ESL Season VI Premier Division Participant',
+				   'ESL Season VII Division 1 1st Place',
+				   'ESL Season VII Division 1 2nd Place',
+				   'ESL Season VII Division 1 3rd Place',
+				   'ESL Season VII Division 2 1st Place',
+				   'ESL Season VII Division 2 2nd Place',
+				   'ESL Season VII Division 2 Participant',
+				   'ESL Season VII Division 3 Participant',
+				   'ESL Season VII Division 4 1st Place',
+				   'ESL Season VII Division 4 2nd Place',
+				   'ESL Season VII Division 4 3rd Place',
+				   'ESL Season VII Division 4 Participant',
+				   'ESL Season VII Division 5 1st Place',
+				   'ESL Season VII Division 5 2nd Place',
+				   'ESL Season VII Division 5 3rd Place',
+				   'ESL Season VII Division 5 Participant',
+				   'ESL Season VII Premiership Division 1st Place',
+				   'ESL Season VII Premiership Division 2nd Place',
+				   'ESL Season VII Premiership Division 3rd Place',
+				   'ESL Season VII Premiership Division Participant']
 
 regionsDict['medal'] = [item for item in regionsDict['medal'] if not item in medalsException]
 regionsDict['medal'].append('Tournament Medal - ETF2L Highlander Tournament')
 regionsDict['medal'].append('Tournament Medal - GWJ Tournament')
 regionsDict['medal'].append('Tournament Medal - UGC Highlander Tournament')
 
-# Fix Essential Accessories
 for region in regionsDict:
-	try:
+	# Fix Essential Accessories
+	if 'The Essential Accessories' in regionsDict[region]:
 		regionsDict[region].remove('The Essential Accessories')
 		regionsDict[region].append('Essential Accessories')
-	except:
-		pass
+	# Remove un-equippable Voodoo-Cursed Soul
+	if 'Voodoo-Cursed Soul' in regionsDict[region]:
+		regionsDict[region].remove('Voodoo-Cursed Soul')
 
 regionsDict = sorted(regionsDict.items())
-output = """"""
+output = open('equipregions.txt', 'wb')
 
-for region in regionsDict:
-	regionname = region[0]
-	regionitems = region[1]
+for regionname, regionitems in regionsDict:
 	regionitems.sort(key=lambda s: (s.lower(), s))
-	output += """\n! {{item name|er """ + regionname + """}}"""
+	output.write('! {{item name|er ' + regionname + '}}')
 	n = 1
 	for item in regionitems:
 		if len(regionitems) == 1:
 			if item == 'Halloween Masks':
-				output += """\n| style="font-weight:bold; font-size:0.95em;" | [[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]"""
+				output.write('\n| style="font-weight:bold; font-size:0.95em;" | [[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]')
 			else:
-				output += """\n| style="font-weight:bold; font-size:0.95em;" | {{item nav link|""" + item + """}}"""
+				output.write('\n| style="font-weight:bold; font-size:0.95em;" | {{item nav link|' + item + '}}')
 		elif n == 1:
 			if item == 'Halloween Masks':
-				output += """\n| style="font-weight:bold; font-size:0.95em;" | [[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]<!--"""
+				output.write('\n| style="font-weight:bold; font-size:0.95em;" | [[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]<!--')
 			else:
-				output += """\n| style="font-weight:bold; font-size:0.95em;" | {{item nav link|""" + item + """}}<!--"""
+				output.write('\n| style="font-weight:bold; font-size:0.95em;" | {{item nav link|' + item + '}}<!--')
 			n += 1
 		elif n == len(regionitems):
 			if item == 'Halloween Masks':
-				output += """\n --><br />[[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]"""
+				output.write('\n --><br />[[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]')
 			else:
-				output += """\n --><br />{{item nav link|""" + item + """}}"""
+				output.write('\n --><br />{{item nav link|' + item + '}}')
 		else:
 			if item == 'Halloween Masks':
-				output += """\n --><br />[[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]<!--"""
+				output.write('\n --><br />[[File:Heavy Mask.png|40px]] [[Halloween Masks{{if lang}}|{{item name|Halloween Masks}}]]<!--')
 			else:
-				output += """\n --><br />{{item nav link|""" + item + """}}<!--"""
+				output.write('\n --><br />{{item nav link|' + item + '}}<!--')
 			n += 1
-	output += """\n|-"""
-output += """\n|}"""
-
-# Save to file
-h = open('equipregions.txt', 'wb')
-h.write(output.encode('utf8'))
-h.close()
+	output.write('\n|-\n')
+output.write('|}\n')
+output.close()
 print '\nDone, printed to equipregions.txt' 
