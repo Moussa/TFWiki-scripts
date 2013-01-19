37a614d99e8d0044959d586530bfb1b80a5ba64e
Moussekateer <moussa.uk@gmail.com>
2013-01-19 02:54:23 +0000

    Add user age check, tidied up code

--- xchat_spammer_killer.py
+++ xchat_spammer_killer.py
@@ -1,70 +1,91 @@
-__module_name__ = "Wiki spam blocker"
-__module_version__ = "1.0"
+__module_name__ = "Wiki Spam Killer"
+__module_version__ = "1.1"
 __module_description__ = "Blocks wiki spammers and deletes pages created"
 
+import threading, sys, datetime, pytz
 import xchat
 import wikitools
-import threading
+
 
 USERNAME = ''
 PASSWORD = ''
 
 WIKI_API = r'http://wiki.tf2.com/w/api.php'
 
-wiki = wikitools.Wiki(WIKI_API)
-wiki.login(username=USERNAME, password=PASSWORD)
-
-def block_user(user):
-	user.block(reason='Spamming links to external sites', expiry=False, nocreate=True, autoblock=True)
-
-def get_user_edits(user):
-	params = {
-		'action': 'query',
-		'list': 'usercontribs',
-		'ucuser': 'User:' + user,
-		'uclimit': '100',
-		'ucprop': 'title|flags',
-		}
-
-	req = wikitools.api.APIRequest(wiki, params)
-	res = req.query(querycontinue=True)
-
-	return res['query']['usercontribs']
-
-def get_created_pages(contribs):
-	titles = []
-	for edit in contribs:
-		if u'new' in edit:
-			titles.append(edit['title'])
-	return titles
-
-def delete_pages(pages):
-	for page in pages:
-		wikitools.Page(wiki, page).delete('Spam')
-
-def activate_killer(word, word_eol, userdata):
-	thread = KillerThread()
-	thread.kill_spammer(word, word_eol, userdata)
-
 
 class KillerThread(threading.Thread):
+	def __init__(self):
+		xchat.prnt('Wiki Spam Killer started')
+		self.wiki = wikitools.Wiki(WIKI_API)
+		self.loggedin = False
+
+	def login(self):
+		if self.wiki.login(username=USERNAME, password=PASSWORD):
+			self.loggedin = True
+			xchat.prnt('Succesfully logged into wiki')
+		else:
+			xchat.prnt('Error: failed to log into wiki. Please reload module with correct login details')
+
+	def block_user(self, user):
+		user.block(reason='Spamming links to external sites', expiry=False, nocreate=True, autoblock=True)
+
+	def get_user_edits(self, user):
+		params = {
+			'action': 'query',
+			'list': 'usercontribs',
+			'ucuser': 'User:' + user,
+			'uclimit': '100',
+			'ucprop': 'title|flags',
+			}
+
+		req = wikitools.api.APIRequest(self.wiki, params)
+		res = req.query(querycontinue=True)
+
+		return res['query']['usercontribs']
+
+	def get_created_pages(self, contribs):
+		titles = []
+		for edit in contribs:
+			if u'new' in edit:
+				titles.append(edit['title'])
+		return titles
+
+	def delete_pages(self, pages):
+		for page in pages:
+			wikitools.Page(self.wiki, page).delete('Spam')
+
+	def is_old_user(self, user):
+		now = datetime.datetime.now(pytz.utc)
+		age = now - user.registration
+		return age.days > 1
 
 	def kill_spammer(self, word, word_eol, userdata):
+		if not self.loggedin:
+			xchat.prnt('Error: please reload module with correct login details')
+			return None
 		if len(word) < 2:
 			xchat.prnt('Error: username was not supplied')
-			return
+			return None
 
 		username = word_eol[1]
-		user = wikitools.User(wiki, username)
-		if not user.isBlocked():
-			block_user(user)
+		user = wikitools.User(self.wiki, username)
+		if self.is_old_user(user):
+			xchat.prnt('User:' + username + ' is > 1 day old. Backing off.')
+		elif not user.isBlocked():
+			self.block_user(user)
 			xchat.prnt('User:' + username + ' has been blocked')
 
-			edits = get_user_edits(username)
-			pagescreated = get_created_pages(edits)
-			delete_pages(pagescreated)
+			edits = self.get_user_edits(username)
+			pagescreated = self.get_created_pages(edits)
+			self.delete_pages(pagescreated)
 		else:
 			xchat.prnt('User:' + username + ' has already been blocked')
 
 
-xchat.hook_command('block', activate_killer)
+def kill_spammer(word, word_eol, userdata):
+	thread.kill_spammer(word, word_eol, userdata)
+
+thread = KillerThread()
+thread.login()
+
+xchat.hook_command('block', kill_spammer)
