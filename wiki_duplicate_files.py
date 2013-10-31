2e592ab1078cc38836d38425082d03951f00d8c7
Moussekateer <moussa.uk@gmail.com>
2013-10-31 04:15:34 +0000

    update script

--- wiki_duplicate_files.py
+++ wiki_duplicate_files.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import wikitools
 
+WIKI_API = 'http://wiki.tf2.com/w/api.php'
+wiki = wikitools.Wiki(WIKI_API)
+
 def get_images(wiki):
 	params = {'action': 'query',
               'generator': 'allimages',
@@ -15,21 +18,27 @@ def get_images(wiki):
 
 
 images = get_images(wiki)
+seen = []
 dupes = {}
 
-for image in images:
-	if 'duplicatefiles' in images[image]:
-		filename = images[image]['title']
-		dupefiles = [u'File:'+_file['name'] for _file in images[image]['duplicatefiles']]
+for _image in images:
+	image = images[_image]
+	filename = image['title'].replace('_', ' ')
+	if 'duplicatefiles' in image and filename not in seen:
+		dupefiles = ['File:'+_file['name'].replace('_', ' ') for _file in image['duplicatefiles']]
+		for _file in dupefiles:
+			if _file not in seen:
+				seen.append(_file)
 		dupes[filename] = dupefiles
+		seen.append(filename)
 
 output = ''
-for dupe in dupes:
+for dupe in sorted(dupes):
 	output += '<gallery>\n'
 	output += '{0}|{1}\n'.format(dupe, dupe[5:])
 	for duplicate in dupes[dupe]:
 		output += '{0}|{1}\n'.format(duplicate, duplicate[5:])
 	output += '</gallery>\n'
 
-with open('Wiki_dupe_files_output.txt', 'wb') as f:
+with open('wiki_dupe_files_output.txt', 'wb') as f:
 	f.write(output)
