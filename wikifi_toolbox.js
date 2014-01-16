23e47260f98afee01b93d51f9499763c9bda62b7
Moussekateer <Moussekateer@users.noreply.github.com>
2014-01-16 16:28:13 +0000

    fix username

--- wikifi_toolbox.js
+++ wikifi_toolbox.js
@@ -1,12 +1,10 @@
 // Inserts a Wiki-Fi link to the page title when viewing userpages
 
 function AddWikiFiLinkToUser() {
-    if (wgPageName.substring(0, 4) != 'User') {
-        return;
+    if (wgCanonicalNamespace == 'User') {
+        var link = ' <span style="font-size:12px">(<a href="http://stats.wiki.tf/user/tf/' + wgTitle.split("/")[0] + '" target="_blank">stats</a>)</span>'
+        $("#firstHeading span").append(link);
     }
-    
-    var link = ' <span style="font-size:12px">(<a href="http://stats.wiki.tf/user/tf/' + wgTitle + '" target="_blank">stats</a>)</span>'
-    $("#firstHeading span").append(link);
 }
 
 addOnloadHook(AddWikiFiLinkToUser);
